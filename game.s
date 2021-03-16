e1f2eca652b5b902a7b04c6c0875f0a51a09a8a1
Nyanserad <oskar.hogberg@gmail.com>
2021-03-16 16:03:17 +0000

    collisions in game

--- game.s
+++ game.s
@@ -64,7 +64,7 @@ gap_pages:
 level_1:
 	db  1,2,3,4,2,6,3,1,7,6,7,3,2,5,4,3,7,1,5,1,5,2,7,0xa    ; level with terminator
 ;	db  1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0xa    ; level with terminator	
-	level_1_len	    equ 24
+	level_1_len	    equ 2
 	    
 level_2:
 	db  2,3,2,6,1,2,7,5,4,5,3,1,5,1,2,3,1,5,1,3,6,4,7,0xa    ; level with terminator
